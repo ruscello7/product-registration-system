341b14331d5536a3a05394e83b850bdca294462d
Matheus Ribeiro <ribeiro7aith@gmail.com>
2026-06-07 21:52:33 -0300

    feat: add multiple products to main

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,15 @@ public class Main {
         System.out.println(p1.price);       // prints the product price
         System.out.println(p1.quantity);        // prints the product quantity
 
+        Product p2 = new Product("Tape", 6.99, 25);     // Create a second product
+        System.out.println(p2.name);        // prints the second product name
+        System.out.println(p2.price);       // prints the second product price
+        System.out.println(p2.quantity);    // prints the second product quantity
+
+        Product p3 = new Product("Alcohol", 9.99, 10);      // Create a third product
+        System.out.println(p3.name);        // prints the third product name
+        System.out.println(p3.price);       // prints the third product price
+        System.out.println(p3.quantity);    // prints the third product quantity
+
     }
 }
